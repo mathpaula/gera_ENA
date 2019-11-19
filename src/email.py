6e1fb76d5e25557fa958ba1eafdb4c4f4b526eb9
mazucanti <daniel.mazucanti.ismart@gmail.com>
2019-11-19 17:55:08 -0300

    terminar códigos nos modelos do ena por bacia

--- src/email.py
+++ src/email.py
@@ -6,11 +6,12 @@ from email.mime.multipart import MIMEMultipart
 COMMASPACE = ', '
 
 msg = MIMEMultipart()
-msg['Subject'] = 'Testecito'
+msg['Subject'] = 'Relatório'
 msg['From'] = "skoposbot@gmail.com"
-msg['To'] = COMMASPACE.join( ["henrique.calogeras@skoposenergia.com.br", "daniel.mazucanti@skoposenergia.com.br"])
+msg['To'] = COMMASPACE.join( ["henrique.calogeras@skoposenergia.com.br", 
+   "lucas.vallim@skoposenergia.com.br", "daniel.mazucanti@skoposenergia.com.br"])
 
-txt = MIMEText('Segue anexo o gráfico da ENA dos últimos 15 dias por submercado')
+txt = MIMEText('Segue ENA por submercado em anexo')
 msg.attach(txt)
 
 with open('../ena.png', 'rb') as fp:
--- src/ena.py
+++ src/ena.py
@@ -59,44 +59,40 @@ def exporta_ena():
     
     
 # %%
-    
-def get_soma_sub_mer(cod_sub_mer):
-    ena = calc_ena()
+
+
+def ena_mercados(ena):
     local = Path('ex_csv/postos.csv')
     postos = pd.read_csv(local, index_col = 0)
-    submercado = ena.join(postos.query('sub_mer == @cod_sub_mer'), on = 'posto', how = 'inner')
-    col = ena.T.head(30).index
-    soma = pd.DataFrame(index = ['ENA '+cod_sub_mer], columns = col)
-    for i in range(30):
-        soma.iloc[0,i] = submercado.iloc[:,i].sum()
-    return soma
-
-# %%
-
-
-def ena_mercados():
-    subSE = get_soma_sub_mer("SE")
-    subS = get_soma_sub_mer("S")
-    subNE = get_soma_sub_mer("NE")
-    subN = get_soma_sub_mer("N")
-    ena_sub = pd.concat([subSE, subS, subNE, subN ])
-    return ena_sub
+    ena_por_mercado = 
+    return ena_m
 
 
 # %%
     
 
-def ena_ree():
-    ena = calc_ena()
+def ena_ree(ena):
     local = Path('ex_csv/postos.csv')
     postos = pd.read_csv(local, index_col = 0)    
     col = ena.T.head(30).index
-    ind = np.arrange(1,13)
+    ind = np.arange(1,13)
     ree = pd.DataFrame(index = ind, columns = col)
-    for i in range(1,13):
-        key = str(i)
-        ree_ena = ena.join(postos.query('ree == @key'), on = 'posto', how = 'inner')
+    for i in np.arange(1,13):
+        ree_ena = ena.join(postos.query('ree == @i'), on = 'posto', how = 'inner')
         for j in range(30):
-            temp= ree_ena.iloc[:,i-1].sum()
-            ree.iloc[i-1,j] = temp
+            ree.iloc[i-1,j] = ree_ena.iloc[:,j].sum()
     return ree
+
+
+# %%
+    
+
+def ena_bacia(ena):
+    local = Path('ex_csv/postos.csv')
+    postos = pd.read_csv(local, index_col = 0)
+    ena_por_bacia = pd.concat([ena, postos], axis=1)
+    ena_por_bacia.drop(['nome','ree','tipo','sub_mer'], axis=1, inplace = True)
+    ena_b = ena_por_bacia.groupby(['bacia']).sum()
+    return ena_b
+
+
