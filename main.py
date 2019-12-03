784e25b57c8a4da0d4cc837197e9687664a231f6
mazucanti <daniel.mazucanti.ismart@gmail.com>
2019-12-03 18:01:07 -0300

    OK A CARGA TÁ TODA ERRADA

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ Created on Mon Nov 25 15:28:24 2019
 
 from src.saida import ena, ipdo, carga
 
-ena_geral = ena.calc_ena()
-ena_submercado = ena.ena_mercados(ena_geral)
-ena_bacias = ena.ena_bacia(ena_geral)
-ena_ree = ena.ena_ree(ena_geral)
-
-ena_submercado.sort_index(ascending = False, inplace = True)
-
-ena.exporta_ena(ena_geral, 'ENA')
-ena.exporta_ena(ena_submercado, 'ENA_Sub_Mer')
-ena.exporta_ena(ena_ree, 'ENA_REE')
-ena.exporta_ena(ena_bacias, 'ENA_Bacias')
-ipdo.exporta_excel()
+#ena_geral = ena.calc_ena()
+#ena_submercado = ena.ena_mercados(ena_geral)
+#ena_bacias = ena.ena_bacia(ena_geral)
+#ena_ree = ena.ena_ree(ena_geral)
+#
+#ena_submercado.sort_index(ascending = False, inplace = True)
+#
+#ena.exporta_ena(ena_geral, 'ENA')
+#ena.exporta_ena(ena_submercado, 'ENA_Sub_Mer')
+#ena.exporta_ena(ena_ree, 'ENA_REE')
+#ena.exporta_ena(ena_bacias, 'ENA_Bacias')
+#ipdo.exporta_ipdo()
+carga.exporta_carga('atual')
+carga.exporta_carga('anterior')
+carga.compara_cargas()
--- src/saida/carga.py
+++ src/saida/carga.py
@@ -11,15 +11,15 @@ import pandas as pd
 from pathlib import Path
 
 
-def importa_carga():
-    local = Path('entradas/carga/CargaDecomp_PMO_Dezembro19')
+def importa_carga(opt:str):
+    
+    local = Path('entradas/carga/carga_'+opt)
     with open(local) as fp:
         carga_bruto = fp.read()
     return carga_bruto
 
 
-def limpa_carga():
-    carga_bruto = importa_carga()
+def limpa_carga(carga_bruto):
     carga_por_estag = carga_bruto.split("&")
     for i in range(5):
         carga_por_estag.pop(0)
@@ -27,8 +27,7 @@ def limpa_carga():
     return carga_por_estag
 
 
-def separa_estags():
-    carga_por_estag = limpa_carga()
+def separa_estags(carga_por_estag):
     carga = []
     for item in carga_por_estag:
         carga.append(item.split('DP'))
@@ -40,11 +39,10 @@ def separa_estags():
             item[i] = texto.split()
     return carga
 
-def organiza_tabela():
+def organiza_tabela(carga):
     ind = ['Estágio 1','Estágio 2','Estágio 3','Estágio 4','Estágio 5']
     col = ["SE","S","NE","N"]
-    carga = separa_estags()
-    carga_decomp = pd.DataFrame(0, index=ind, columns = col)
+    carga_decomp = pd.DataFrame(0, index = ind, columns = col)
     cargahora = 0
     horas = 0
     for i in range(5):
@@ -53,8 +51,30 @@ def organiza_tabela():
                 cargahora += float(carga[i][j][2*k+1]) * float(carga[i][j][2+2*k])
                 horas += float(carga[i][j][2+2*k])
             carga_decomp.iloc[i][j] = cargahora/horas
-            cargahora = horas = 0
+            cargahora = 0
+            horas = 0
     return carga_decomp
 
-a = separa_estags()
-b = organiza_tabela()
+
+def exporta_carga(opt:str):
+    carga_bruto = importa_carga(opt)
+    carga_por_estag = limpa_carga(carga_bruto)
+    carga = separa_estags(carga_por_estag)
+    carga_decomp = organiza_tabela(carga)
+    local = Path('saídas/carga/carga_'+opt+'.xls')
+    carga_decomp.to_excel(local)
+    
+    
+#def compara_cargas():
+#    local_at = Path('saídas/carga/carga_atual.xls')
+#    local_ant = Path('saídas/carga/carga_anterior.xls')
+#    lin = ['2 e 1', '3 e 2', '4 e 3', '5 e 4']
+#    col = ["SE","S","NE","N"]
+#    difer = pd.DataFrame(0, index = lin, columns = col)
+#    c_at = pd.read_excel(local_at, index_col = 0)
+#    c_ant = pd.read_excel(local_ant, index_col = 0)
+#    for i in range(4):
+#        for j in range(4):
+#            
+            
+    
